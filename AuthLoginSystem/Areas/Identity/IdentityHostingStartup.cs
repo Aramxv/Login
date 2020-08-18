61049e7a1535b3bcfe5dddcf988f8433ec7710bf
Aramxv <reijraxyr@gmail.com>
2020-08-18 17:00:36 +0800

    Initial Commit

---
+++ AuthLoginSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AuthLoginSystem.Areas.Identity.Data;
+using AuthLoginSystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthLoginSystem.Areas.Identity.IdentityHostingStartup))]
+namespace AuthLoginSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthLoginSystemDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthLoginSystemDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    // Turn off the Lower-UpperCase and Non Alphanumeric Char 
+                    // in Password Requirements
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<AuthLoginSystemDbContext>();
+            });
+        }
+    }
+}
---
+++ AuthLoginSystem/Models/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthLoginSystem.Models
+{
+    public class IndexModel : PageModel
+    {
+       
+    }
+}
